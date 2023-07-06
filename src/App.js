02e694d3db09b2e7982267d39ec7e03f4c4cb337
Ishaan Joshi <joshi.ishaan.2001@gmail.com>
2023-07-06 23:11:02 +0530

    Mapping Tasks

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import './App.css';
 import TaskForm from './Components/Tasks/TaskForm'
 import Task from './Components/Tasks/Task'
+import { useState } from 'react';
 
 function App() {
+	const [tasks, setTasks] = useState([]);
+
+	const addTask = (name) => {
+		setTasks(previous => {
+			return [...previous, {
+				name: name,
+				done: false
+			}];
+		})
+	}
 	return (
 		<div className="main">
-			<TaskForm />
-			<Task />
-			<Task />
-			<Task />
-			<Task />
+			<TaskForm onAdd={addTask} />
+
+			{
+				tasks.map((task, index) => (
+					<Task />
+				))
+			}
+
 		</div>
 	);
 }

e5dac318c245c8810cdae735aef28eeec0f0ea43
Ishaan Joshi <joshi.ishaan.2001@gmail.com>
2023-07-06 19:53:21 +0530

    Label for Individual Task

---
+++ src/Components/Tasks/Task.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Task = () => {
+  return (
+    <div className='task'>
+        <input type="checkbox" />
+        Testing Task
+    </div>
+  )
+}
+
+export default Task

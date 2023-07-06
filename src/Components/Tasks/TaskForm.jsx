93edd83da3bd272e003f8cbb75c3140876f42698
Ishaan Joshi <joshi.ishaan.2001@gmail.com>
2023-07-06 23:10:45 +0530

    Loading Task into Array

--- src/Components/Tasks/TaskForm.jsx
+++ src/Components/Tasks/TaskForm.jsx
@@ -1,18 +1,23 @@
 import React, { useState } from 'react'
 
-const TaskForm = () => {
+const TaskForm = ({ onAdd }) => {
 	const [taskName, setTaskName] = useState('')
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		onAdd(taskName);
+		setTaskName('')
+	}
+
 	return (
-		<div>
-			<form>
-				<button>+</button>
-				<input
-					type="text"
-					placeholder='Your Next Task!'
-					onChange={(e) => setTaskName(e.target.value)}
-				/>
-			</form>
-		</div>
+		<form onSubmit={handleSubmit}>
+			<button>+</button>
+			<input
+				type="text"
+				placeholder='Your Next Task!'
+				onChange={(e) => setTaskName(e.target.value)}
+			/>
+		</form>
 	)
 }
 
